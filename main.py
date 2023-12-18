d41ec2c2814591f9c5b50d773dafb07be9aa157b
wh04m1 <49116189+randy1369@users.noreply.github.com>
2023-12-18 20:42:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,42 @@
 import streamlit as st
 import json
-import os
+
+
 
 st.set_page_config(layout="wide")
 
 # Function to display information for each tourist place
 def display_place_info(place_name, place_state):
     st.subheader(f"Information about {place_name}")
-  
-
     file_path = f"json-files/{place_state}.json"
 
-    if os.path.exists(file_path):
-        try:
-            with open(file_path, "r") as file:
-                place_data = json.load(file)
-                        # Create tabs dynamically based on the available data
-                tabs = st.tabs([attr for attr in place_data[place_name]])
-                
-
-                # Display information for each tab
-                for tab_name, tab_data in zip(tabs, place_data[place_name].values()):
-                    
-                    with tab_name:
-                        
-                        # You can customize the content for each tab based on tab_data
-                        with st.expander("About-", expanded=True):
-                            st.write(tab_data['description'])
-                            image_url = tab_data['image']
-                            st.image(image_url, width=600)
-
-                        with st.expander("Activities-"):
-                            st.table(tab_data['activities'])
-
-                        with st.expander("Accommadation-"):
-                            st.table(tab_data['accommodation'])
-
-                        with st.expander("Transportaion-"):
-                            st.table(tab_data['transportation'])
-
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON in file '{file_path}': {e}")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-    else:
-        print(f"Error: File '{file_path}' not found.")
+    with open(file_path, "r") as file:
+        place_data = json.load(file)
 
        
+        # Create tabs dynamically based on the available data
+        tabs = st.tabs([attr for attr in place_data[place_name]])
+        
 
+        # Display information for each tab
+        for tab_name, tab_data in zip(tabs, place_data[place_name].values()):
+            
+            with tab_name:
+                
+                # You can customize the content for each tab based on tab_data
+                with st.expander("About-", expanded=True):
+                    st.write(tab_data['description'])
+                    image_url = tab_data['image']
+                    st.image(image_url, width=600)
+
+                with st.expander("Activities-"):
+                    st.table(tab_data['activities'])
+
+                with st.expander("Accommadation-"):
+                    st.table(tab_data['accommodation'])
+
+                with st.expander("Transportaion-"):
+                    st.table(tab_data['transportation'])
 
 
 # Main Streamlit app
@@ -55,7 +44,7 @@ def main():
     st.sidebar.title("Tourist Places")
 
     # Sidebar: Choose the tourist place
-    chosen_state = st.sidebar.selectbox("Choose a state", ["Kerala", "Goa", "Other States"])
+    chosen_state = st.sidebar.selectbox("Choose a state", ["kerala", "Goa", "Other States"])
 
     if chosen_state == "Kerala":
         # If Kerala is chosen, display sub-options
